f2bbbf0cd37c447b9686a50744d55385ab23a244
allowMultiQueries <asd1161575755@outlook.com>
2025-07-13 11:22:49 +0800

    Update geoip2_sync.sh

--- geoip/geoip2_sync.sh
+++ geoip/geoip2_sync.sh
@@ -13,12 +13,12 @@ for cmd in curl; do
 done
 # GitHub源配置
 declare -A database_urls=(
-  # Hackl0us 维护版本
-  [GeoCN]="https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb"
-  # P3TERX 与 Loyalsoldier 维护版本
-  [GeoLite2-ASN]="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country-without-asn.mmdb"
-  [GeoLite2-City]="https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb"
-  [GeoLite2-Country]="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb"
+  # ljxi 维护版本
+  [GeoCN]="https://github.com/ljxi/GeoCN/releases/download/Latest/GeoCN.mmdb"
+  # P3TERX 维护版本
+  [GeoLite2-ASN]="https://git.io/GeoLite2-ASN.mmdb"
+  [GeoLite2-City]="https://git.io/GeoLite2-City.mmdb"
+  [GeoLite2-Country]="https://git.io/GeoLite2-Country.mmdb"
 )
 # 下载对应数据库
 download_database() {
